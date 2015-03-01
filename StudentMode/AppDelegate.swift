f629fd3bca1ca0cf10070580e1696c1bdf609607
David Schmetterling <David@Davids-Retina-MacBook-Pro.local>
2015-03-01 01:03:17 -0500

    Initial Commit

---
+++ StudentMode/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StudentMode
+//
+//  Created by David Schmetterling on 3/1/15.
+//  Copyright (c) 2015 David Schmetterling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
